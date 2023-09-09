3c5fbc08f0215479cb3b7f8222a632277f964447
SakibAlHasan10 <sakibalhasan.me1@gmail.com>
2023-09-09 17:35:27 +0600

    add nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Nav from './allJsxFile/navbar'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+    <Nav></Nav>
       <h1 className='text-5xl color-red-400'>hello</h1>
-      <p className='bg-green-500'>hello</p>
+      <p className='bg-green-500'>hello</p> 
       
       
     </>
---
+++ src/allJsxFile/Navbar.jsx
@@ -0,0 +1,9 @@
+import './Navbar.css'
+export default function Nav(){
+    
+    return (
+    <div className="navbar">
+        <h3 className="navLogo">hello</h3>
+    </div>
+    )
+}
